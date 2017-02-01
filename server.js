6d659ccfda1246e0a4fbfd9ff59fdb5f2280f190
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-01 22:04:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one', function(req,res){
+    res.send('Article 1 requested and it will be served here');
+});
+
+app.get('article-two', function(req,res){
+    res.send('Article 2 requested and it will be served here');
+});
+
+app.get('article-three', function(req,res){
+    res.send('Article 3 requested and it will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
