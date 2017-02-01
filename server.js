e38ed5fe01cdfd6afba51722ed7c3b56a5421ce4
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-01 19:11:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var hi=prompt('Hello');
+var hi=prompt("Hello");
 var app = express();
 app.use(morgan('combined'));
 
