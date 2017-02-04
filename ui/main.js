a787e0e4c7d067e6bea019b654664b0e914d0269
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-04 13:01:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,12 @@ element.innerHTML='Hello';
 
 //Move the image
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft= marginLeft+'px';  
+}
 img.onclick= function(){
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft ='100px';
 };
