f9a15dc92fd8c28f73f4b8d1b0bb310664f548fd
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-04 16:34:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-//Change the text of center-text div
+/*//Change the text of center-text div
 
 var element= document.getElementById('center-text');
 
@@ -16,4 +16,19 @@ function moveRight(){
 img.onclick= function(){
     var interval=setInterval(moveRight,50);
     //img.style.marginLeft ='100px';
+};*/
+
+var button =document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+    //Make a request to counter endpoint
+    
+    //Capture Response and store it in a variable
+    
+    //Render the variable in correct span
+    counter+=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+      
 };
