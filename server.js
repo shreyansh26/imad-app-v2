ff434b527d3b914fe14380d1a81c08e184c8545c
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-04 12:26:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName', function(req,res){
     //articleName === article-one
     //articles[articleName]={} content object for article one
