db9265cc042b4c6836fde4b6646a1f60f565083b
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-02-28 01:23:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,13 +131,25 @@ app.get('/submit-name', function (req, res) { //URL: /submit-name?name=xxxxx
   
 });
 
-app.get('/:articleName', function(req,res){
+app.get('/articles/:articleName', function(req,res){
     //articleName === article-one
     //articles[articleName]={} content object for article one
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title=" +req.params.articleName,function(req,res){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+        if(result.rows.length===0){
+            res.status(404).send('Article not found');
+        }else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+       }
+    });
+    //res.send(createTemplate(articles[articleName]));
 });
-
+/*
 app.get('/article2', function(req,res){
    res.send(createTemplate(articleTwo));
 });
@@ -145,7 +157,7 @@ app.get('/article2', function(req,res){
 app.get('/article3', function(req,res){
     res.send(createTemplate(articleThree));
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
