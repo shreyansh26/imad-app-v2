3d7bdb16946649e88bf0654af1ed636179088728
shreyansh26 <shreyansh.pettswood@gmail.com>
2017-03-03 16:04:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,8 @@ app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
 });
-/*
+
+var pool = new Pool(config);
 app.post('/create-user',function(req,res){
    //username, password 
    var username = req.body.username;
@@ -127,8 +128,8 @@ app.post('/create-user',function(req,res){
       }
    });
 });
-*/
-var pool = new Pool(config);
+
+
 
 app.get('/test-db',function(req,res){
    //Make a select request
